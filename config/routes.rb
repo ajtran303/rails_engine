259438db5d6671d326a08f8b4fb3a670a6da9e3f
AJ Tran <ajtrandev@gmail.com>
2020-09-02 14:21:22 -0600

    Expose endpoint for Getting a Merchant's Items

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :items, only: [:show, :index]
       resources :merchants, only: [:show, :index]
+
+      namespace :merchants do
+        get '/:id/items', to: 'items#index'
+      end
+
+      # namespace :items do
+      #   get '/:id/merchant', to: 'merchants#index'
+      # end
     end
   end
 end
