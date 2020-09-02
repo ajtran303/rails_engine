240eb769cf1fd98ba8d7ee032cbeaccbbdbaf76f
AJ Tran <ajtrandev@gmail.com>
2020-09-02 16:39:24 -0600

    Add merchant method #total_revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,14 @@ class Merchant < ApplicationRecord
 
   has_many :items
   has_many :invoices
+
+  def total_revenue
+    select_sql = "invoices.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue"
+    merchant_invoices = self.invoices.select(select_sql)
+      .joins(:invoice_items, :purchases)
+      .where(purchases: {result: "success"}, invoices: {status: "shipped"})
+      .group(:id)
+
+    merchant_invoices.sum(&:revenue)
+  end
 end
