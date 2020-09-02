a719837f3873e07c9b03a037cc41d2433dc4a2ac
AJ Tran <ajtrandev@gmail.com>
2020-09-02 16:54:53 -0600

    Refactor merchant#total_revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,10 +5,13 @@ class Merchant < ApplicationRecord
   has_many :invoices
 
   def total_revenue
-    select_sql = "invoices.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue"
-    merchant_invoices = self.invoices.select(select_sql)
+    sum_aggregate = "SUM(invoice_items.quantity * invoice_items.unit_price)"
+    select_query = "invoices.*, #{sum_aggregate} AS revenue"
+
+    merchant_invoices = self.invoices.select(select_query)
       .joins(:invoice_items, :purchases)
-      .where(purchases: {result: "success"}, invoices: {status: "shipped"})
+      .merge(Purchase.successful)
+      .merge(Invoice.shipped)
       .group(:id)
 
     merchant_invoices.sum(&:revenue)
