7176ae7d54fd4e5b9c5320fed6b0065b30285199
AJ Tran <ajtrandev@gmail.com>
2020-09-02 11:52:06 -0600

    Expose endpoint for getting all Items

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::ItemsController < ApplicationController
   def show
     render json: ItemSerializer.new(Item.find(params[:id])).serialized_json
   end
+
+  def index
+    render json: ItemSerializer.new(Item.all).serialized_json
+  end
 end
