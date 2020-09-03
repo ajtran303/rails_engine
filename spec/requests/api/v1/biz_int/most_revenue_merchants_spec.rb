8dbe910ff4646d183b697c8bc81256d94f575c75
AJ Tran <ajtrandev@gmail.com>
2020-09-02 23:49:37 -0600

    Add most_revenue_merchants_spec.rb

---
+++ spec/requests/api/v1/biz_int/most_revenue_merchants_spec.rb
@@ -0,0 +1,66 @@
+require "rails_helper"
+
+RSpec.describe "Get Merchants with Most Revenue API" do
+  def parse_body(response)
+    JSON.parse(response.body, symbolize_names: :true)
+  end
+
+  before :each do
+    @merchant2 = create(:merchant)
+    item = create(:item, merchant: @merchant2, unit_price: 100.0)
+    invoice = create(:invoice, merchant: @merchant2, status: "shipped")
+    sale = create(:invoice_item, invoice: invoice, item: item, quantity: 2, unit_price: 100.0)
+    purchase = create(:purchase, invoice: invoice, result: "success")
+
+    @merchant1 = create(:merchant)
+    item = create(:item, merchant: @merchant1, unit_price: 100.0)
+    invoice = create(:invoice, merchant: @merchant1, status: "shipped")
+    sale = create(:invoice_item, invoice: invoice, item: item, quantity: 2, unit_price: 90.0)
+    purchase = create(:purchase, invoice: invoice, result: "success")
+  end
+
+  it "sends the merchant with most revenue" do
+    get "/api/v1/merchants/most_revenue?quantity=#{1}"
+    expect(response).to be_successful
+
+    json = parse_body(response)
+
+    expected =
+    {
+      "data": [
+        {
+          :id=>"#{@merchant2.id}",
+          :type=>"merchant",
+          :attributes=>{:name=>"#{@merchant2.name}"}
+        }
+      ]
+    }
+
+    expect(json).to eq(expected)
+  end
+
+  it "sends the merchant with most revenue" do
+    get "/api/v1/merchants/most_revenue?quantity=#{2}"
+    expect(response).to be_successful
+
+    json = parse_body(response)
+
+    expected =
+    {
+      "data": [
+        {
+          :id=>"#{@merchant2.id}",
+          :type=>"merchant",
+          :attributes=>{:name=>"#{@merchant2.name}"}
+        },
+        {
+          :id=>"#{@merchant1.id}",
+          :type=>"merchant",
+          :attributes=>{:name=>"#{@merchant1.name}"}
+        }
+      ]
+    }
+
+    expect(json).to eq(expected)
+  end
+end
