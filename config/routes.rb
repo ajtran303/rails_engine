60f7e6ad5c8ba0c4a43954095f72afebcc3b7873
AJ Tran <ajtrandev@gmail.com>
2020-09-02 21:27:42 -0600

    Add route for finding single item

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
+
   namespace :api do
     namespace :v1 do
+
+      namespace :items do
+        get '/:id/merchant', to: 'merchants#index'
+        get '/find', to: 'search#show'
+      end
+
       resources :items, only: [:show, :index]
       resources :merchants, only: [:show, :index]
 
       namespace :merchants do
         get '/:id/items', to: 'items#index'
         get '/:id/revenue', to: 'biz_int#merchant_revenue'
-      end
-
-      namespace :items do
-        get '/:id/merchant', to: 'merchants#index'
       end
     end
   end
