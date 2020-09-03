520707cd7b243d46f8f548e02fa001d1131a826d
AJ Tran <ajtrandev@gmail.com>
2020-09-03 00:58:40 -0600

    Add items_create_spec.rb

---
+++ spec/requests/api/v1/crud/items_create_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe "Create Items CRUD API" do
+  def parse_body(response)
+    JSON.parse(response.body, symbolize_names: :true)
+  end
+
+  it "can create an item" do
+    new_item = build_stubbed(:item)
+    expect(Item.count).to eq(0)
+
+    item_params = {
+                    name: new_item.name,
+                    description: new_item.description,
+                    unit_price: new_item.unit_price,
+                    merchant_id: new_item.merchant_id
+                  }
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/items", headers: headers, params: JSON.generate({item: item_params})
+
+    expect(Item.count).to eq(1)
+    item = Item.last
+
+    expect(response).to be_successful
+    expect(item.name).to eq(item_params[:name])
+  end
+end
