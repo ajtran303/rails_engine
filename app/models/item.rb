6b20b85729e9ce47aa0303829626965795e19e65
AJ Tran <ajtrandev@gmail.com>
2020-09-02 22:07:22 -0600

    Add search class methods to Item and Merchant models

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,26 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def self.search(params)
+    find_by(params) ||
+    fuzzy_find(params) ||
+    find_by_date(params)
+  end
+
+  def self.fuzzy_find(params)
+    return nil unless params[:name] || params[:description]
+
+    attribute = params.keys.first
+    query_fragment = params[attribute].downcase
+    where("lower(#{attribute}) like ?", "%#{query_fragment}%").first
+  end
+
+  def self.find_by_date(params)
+    return nil unless params[:updated_at] || params[:created_at]
+
+    attribute = params.keys.first
+    date = params[attribute]
+    where("date(#{attribute}) = ?", date).first
+  end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,6 +4,28 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
 
+  def self.search(params)
+    find_by(params) ||
+    fuzzy_find(params) ||
+    find_by_date(params)
+  end
+
+  def self.fuzzy_find(params)
+    return nil unless params[:name]
+
+    attribute = params.keys.first
+    query_fragment = params[attribute].downcase
+    where("lower(#{attribute}) like ?", "%#{query_fragment}%").first
+  end
+
+  def self.find_by_date(params)
+    return nil unless params[:updated_at] || params[:created_at]
+
+    attribute = params.keys.first
+    date = params[attribute]
+    where("date(#{attribute}) = ?", date).first
+  end
+
   def total_revenue
     sum_aggregate = "SUM(invoice_items.quantity * invoice_items.unit_price)"
     select_query = "invoices.*, #{sum_aggregate} AS revenue"
