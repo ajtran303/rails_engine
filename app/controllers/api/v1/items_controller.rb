8e92309544b0ff65b7afc989e463b342879803ac
AJ Tran <ajtrandev@gmail.com>
2020-09-03 01:27:09 -0600

    Expose endpoint for updating item on crud api

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -12,6 +12,12 @@ class Api::V1::ItemsController < ApplicationController
     render json: ItemSerializer.new(item).serialized_json
   end
 
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    render json: ItemSerializer.new(item).serialized_json
+  end
+
   private
 
   def item_params
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
         get '/most_items', to: 'biz_int#most_items'
       end
 
-      resources :items, only: [:show, :index, :create]
+      resources :items, only: [:show, :index, :create, :update]
       resources :merchants, only: [:show, :index, :create]
 
     end
